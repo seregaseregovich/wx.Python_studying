885a8b46429bca431a846e883d3efbb95445ff45
Sergei Sergeevich <sergeisergeevich@mail.ru>
2023-04-02 19:57:15 +0300

    Общая структура интерфейса

--- Files/file_2.py
+++ Files/file_2.py
@@ -8,13 +8,13 @@ class MyFrame(wx.MDIParentFrame):
         menubar = wx.MenuBar()
         self.SetMenuBar(menubar)
 
-        win = wx.MDIChildFrame(self, 0, 'Child Window', size=(200, 150))
+        win = wx.MDIChildFrame(self, -1, 'Child Window', size=(300, 150))
         win.Show()
 
 
 app = wx.App()
 
-frame = MyFrame(None, 'Main Window')
+frame = MyFrame(None, title='Main Window')
 frame.Show()
 
 app.MainLoop()
