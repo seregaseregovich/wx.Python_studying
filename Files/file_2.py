50d48e98008eae6cf1d271c40983e22d72ce0b70
Sergei Sergeevich <sergeisergeevich@mail.ru>
2023-04-09 19:55:18 +0300

    Добавил комментарии

--- Files/file_2.py
+++ Files/file_2.py
@@ -1,20 +1,28 @@
 import wx
 
-
+# класс MDIParentFrame() создает многооконный интерфейс.
+# С ним связан класс MDIChildFrame(), который
+# создает дочерний класс (окно) внутри класса MDIParentFrame.
 class MyFrame(wx.MDIParentFrame):
-    def __init__(self, parent, title):
+    def __init__(self, parent, title):  # конструктор для инициализации
         super().__init__(parent, title=title)
 
-        menubar = wx.MenuBar()  # Метод создает панель, где можно создать меню
+        # Метод MenuBar() создает панель, где можно создать
+        #  меню. Без этого метода класс MDIParentFrame() не работает.
+        menubar = wx.MenuBar()
         self.SetMenuBar(menubar)
 
-        win = wx.MDIChildFrame(self, -1, 'Child Window', size=(300, 150))
-        win.Show()
+        # С помощью класса MDIChildFrame() создаются дочерние окна.
+        win1 = wx.MDIChildFrame(self, -1, 'Child Window1', size=(100, 150))
+        win1.Show()
+
+        win2 = wx.MDIChildFrame(self, -2, 'Child Window2', size=(100, 150))
+        win2.Show()
 
 
 app = wx.App()
 
-frame = MyFrame(None, title='Main Window')
+frame = MyFrame(None, title='Parent Window')
 frame.Show()
 
 app.MainLoop()
